1e4b20be86b31532c7e81b700252dd78cd7ea58a
NYUAD <nyuad@C3-019-IMACP-14.local>
2020-11-12 03:33:34 +0400

    Finishing the site

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 let playButton = document.getElementById("playButton");
 let player = document.getElementById("player");
+let landing = document.getElementById("landing");
+let emojiBox = document.getElementById("emoji-box");
+let filterBW = document.getElementById("overlay");
+let captions = document.getElementById("captions");
+let cc = document.getElementById("cc");
 
 playButton.addEventListener('click', () => {
-	player.play();
+	landing.classList.toggle('fade');
+	state = 1;
+	setTimeout(() => {  startAudio(); }, 2000);
+});
+
+cc.addEventListener('click', () => {
+	captions.classList.toggle('showCaps');
 });
 
 player.addEventListener('timeupdate', () => {
-	console.log(player.currentTime);
+	audioTime = player.currentTime;
 }) ;
+
+function startAudio(){
+	player.play();
+	emojiBox.classList.toggle('show');
+    landing.parentNode.removeChild(landing);
+}
--- sketch.js
+++ sketch.js
@@ -1,8 +1,16 @@
+// Angle of sun
 let theta = 0;
 
 // Time goes up to 100
 let time = 0;
 
+// Variable for smooth time transition
+let goalTime = 0;
+
+// Timestamp of audio when playing
+let audioTime = 0;
+
+// Colors
 let sunNoon = [255, 243, 5];
 let sunMorning = [255, 106, 0];
 
@@ -10,40 +18,75 @@ let morning = [237, 161, 140];
 let noon = [140, 227, 237];
 let evening = [193, 154, 237];
 
-// State to switch between, landing, day A, and day B
+// State to switch between landing, day A, and day B
 // 0 - landing
 // 1 - Day A
 // 2 - Day B
 let state = 0;
 
+// variable for smooth transitions between days
+let transitionDone = false;
+
 function windowResized(){
+	// Responsive canvas
 	resizeCanvas(windowWidth, windowHeight);
 }
 
 function setup() {
 	canvas = createCanvas(windowWidth, windowHeight);
 	canvas.position(0,0);
+	// stay behind everything
 	canvas.style('z-index', '-1');
 }
 
 function draw() {
 	if(state == 0){
+		// Interactive stage
 		time = map(mouseX, 0, width, 0, 100);
 		drawSky();
 		drawSun();
 	}
-	else if(state == 1){
-		time += 0.2;
+	else{
+		// Day A
+		showSketch();
+	}
+	if(state == 2){
+		// Day B
+		filter(GRAY);
+	}
+}
+
+function showSketch(){
+	if(transitionDone){
+		// Transition to goal time if needed
+		if(time < goalTime){
+			time += 0.1;
+		}
+		showEmoji();
+		drawSky();
+		drawSun();
+	}
+	else{
+		// If not yet transitioned,
+		// smoothly move sun to start
+		if(time > 0){
+			time -= 0.4;
+		}
+		else{
+			transitionDone = true;
+		}
 		drawSky();
 		drawSun();
 	}
 }
 
 function drawSun(){
+	// Map angle of sun based on time
 	theta = map(time, 0, 100, 4.25, 5.166);
 
 	push();
 
+	// Adjust color based on time
 	let currentSun = [];
 	if(time < 50){
 		for(let i = 0; i < 3; i++){
@@ -56,16 +99,23 @@ function drawSun(){
 		}
 	}
 	fill(currentSun[0],currentSun[1],currentSun[2]);
+	if(state == 2){
+		fill(100);
+	}
+
 	noStroke();
 
+	// Draw sun along arc path
 	translate(width/2,1.2*height);
 	let x = cos(theta) * width * 1.2;
 	let y = sin(theta) * height;
-	ellipse(x,y,width/20,width/20);
+	ellipse(x,y,width/17,width/17);
+
 	pop();
 }
 
 function drawSky(){
+	// Adjust sun color based on time
 	let currentSky = [];
 	if(time < 50){
 		for(let i = 0; i < 3; i++){
@@ -78,4 +128,93 @@ function drawSky(){
 		}
 	}
 	background(currentSky[0],currentSky[1],currentSky[2]);
+}
+
+function showEmoji(){
+	// Show emojis as visual guides for audio
+	if(audioTime < 11){
+		emojiBox.innerHTML = "<h1>Day A</h1>";
+		captions.innerHTML = "<h2>Hi</h2>";
+	}
+	else if(audioTime < 17){
+		emojiBox.innerHTML = "<h1>⏰</h1>";
+		goalTime = 10;
+		captions.innerHTML = "<h2>asdf</h2>";
+	}
+	else if(audioTime < 30){
+		emojiBox.innerHTML = "<h1>🛁</h1>";
+		goalTime = 15;
+		captions.innerHTML = "<h2>Hasdfasdgi</h2>";
+	}
+	else if(audioTime < 54){
+		emojiBox.innerHTML = "<h1>🧑‍🏫</h1>";
+		goalTime = 25;
+	}
+	else if(audioTime < 64){
+		emojiBox.innerHTML = "<h1>🗒️</h1>";
+		goalTime = 40;
+	}
+	else if(audioTime < 73){
+		emojiBox.innerHTML = "<h1>⌨️</h1>";
+		goalTime = 55;
+	}
+	else if(audioTime < 86){
+		emojiBox.innerHTML = "<h1>📲</h1>";
+		goalTime = 67;
+	}
+	else if(audioTime < 98){
+		emojiBox.innerHTML = "<h1>🍽️</h1>";
+		goalTime = 80;
+	}
+	else if(audioTime < 110){
+		emojiBox.innerHTML = "<h1>📒</h1>";
+		goalTime = 93;
+	}
+	else if(audioTime < 125){
+		// Transition from day A to day B
+		emojiBox.innerHTML = "";
+		if(state == 1){
+			console.log("AAA");
+			state = 2;
+			transitionDone = false;
+			filterBW.classList.toggle('bnw');
+		}
+	}
+	else if(audioTime < 132){
+		emojiBox.innerHTML = "<h1>Day B</h1>";
+		goalTime = 0;
+	}
+	else if(audioTime < 138){
+		emojiBox.innerHTML = "<h1>⏰</h1>";
+		goalTime = 10;
+	}
+	else if(audioTime < 145){
+		emojiBox.innerHTML = "<h1>🛁</h1>";
+		goalTime = 15;
+	}
+	else if(audioTime < 162){
+		emojiBox.innerHTML = "<h1>🧑‍🏫</h1>";
+		goalTime = 35;
+	}
+	else if(audioTime < 176){
+		emojiBox.innerHTML = "<h1>⌨️</h1>";
+		goalTime = 50;
+	}
+	else if(audioTime < 198){
+		emojiBox.innerHTML = "<h1>📲</h1>";
+		goalTime = 67;
+	}
+	else if(audioTime < 213){
+		emojiBox.innerHTML = "<h1>🍽️</h1>";
+		goalTime = 80;
+	}
+	else if(audioTime < 238){
+		emojiBox.innerHTML = "<h1>📒</h1>";
+		goalTime = 93;
+	}
+	else{
+		// Give listener restart link
+		emojiBox.innerHTML = "<h1><a href='example.com'>Restart</a></h1>"
+		goalTime = 100;
+	}
 }
